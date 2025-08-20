b0fc1ee07323248229ffc622e12377e5e60ead13
AlekseyStrog <strogan0@yandex.ru>
2025-08-20 12:04:30 +0300

    Считаем объем при выводе в printf
    Считаем объем при выводе в printf. Переменную для объема не используем.

---
+++ code_7.c
@@ -0,0 +1,16 @@
+/* code_7.c
+Считаем объем при выводе в printf
+*/
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+	setlocale(LC_ALL, "rus");
+
+	int dlina = 10, shirina = 11, visota = 12;
+
+	printf("Длина = %d, Ширина = %d  Высота = %d Объем = %d", dlina, shirina, visota, dlina * shirina * visota);
+
+	return 0;
+}
